98a80406a71171062ff4e0550ee4afdc25300f55
peffles <wyattpefley@gmail.com>
2018-06-18 11:27:40 -0700

    renders stuff to page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,10 +3,10 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import AuthRedirect from '../auth-redirect/auth-redirect';
+// import AuthRedirect from '../auth-redirect/auth-redirect';
 import Dashboard from '../dashboard/dashboard';
-import Header from '../../header/header';
-import AuthLanding from '../auth-landing/auth-landing';
+import Header from '../header/header';
+// import AuthLanding from '../auth-landing/auth-landing';
 
 class App extends React.Component {
   render() {
@@ -15,10 +15,10 @@ class App extends React.Component {
       <BrowserRouter>
         <div>
           <Header/>
-          <Route path='*' component={AuthRedirect}/>
+          {/* <Route path='*' component={AuthRedirect}/>
           <Route exact path='/' component={AuthLanding}/>
           <Route exact path='/signup' component={AuthLanding}/>
-          <Route exact path='/login' component={AuthLanding}/>
+          <Route exact path='/login' component={AuthLanding}/> */}
           <Route exact path='/dashboard' component={Dashboard}/>
         </div>
       </BrowserRouter>
